9e6336223cdf50a5440abd0a4a1a0334b27dd59e
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-07-31 00:54:52 +0200

    removed old drizzle folder

--- src/lib/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/planetscale-serverless'
-import { connect } from '@planetscale/database'
-
-// create database connection
-const connection = connect({
-  url: process.env.DATABASE_URL,
-})
-
-export const db = drizzle(connection)
--- src/lib/db/migrations/0001_noisy_echo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `productCategories` MODIFY COLUMN `name` varchar(256) NOT NULL;--> statement-breakpoint
-ALTER TABLE `productCategories` MODIFY COLUMN `desc` text NOT NULL;

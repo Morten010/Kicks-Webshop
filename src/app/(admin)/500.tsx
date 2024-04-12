3e522c56f367e7e7d29d13ef9de9ebd6b23d89d3
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-04-12 23:32:33 +0200

    added 500 error page

---
+++ src/app/(admin)/500.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface Custom500pageProps {
+  
+}
+
+const Custom500page: FC<Custom500pageProps> = ({}) => {
+  return <div>
+    <h1>
+        Something went wrong
+    </h1>
+  </div>
+}
+
+export default 500
